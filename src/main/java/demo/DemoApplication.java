fe6bae70d179486b69e85b6be2447c186b019b75
Huseyin Bacanak <huseyin.bacanak@tpe.gov.tr>
2016-08-01 16:51:20 +0300

    ok

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,13 +1,17 @@
-package cloud;
+package demo;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableAutoConfiguration
+@Configuration
 public class DemoApplication {
 
 	@Value("${name}")
--- src/test/java/demo/DemoApplicationTests.java
+++ src/test/java/demo/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package cloud;
+package demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
